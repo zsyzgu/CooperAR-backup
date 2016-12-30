367bce93b33213eed21b0466a21ad9f317adb397
zsyzgu <zsyzgu@163.com>
2016-12-30 16:44:46 +0800

    UWP socket for image transport.

--- Assets/Scripts/CaptureSimulator.cs
+++ Assets/Scripts/CaptureSimulator.cs
@@ -1,16 +1,57 @@
 ﻿using UnityEngine;
 using System.IO;
-using System.Net.Sockets;
-using System.Threading;
 using OpenCvSharp;
 using OpenCvSharp.CPlusPlus;
+
+#if WINDOWS_UWP
+using System;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+#else
+using System.Net.Sockets;
+using System.Threading;
+#endif
 
 public class CaptureSimulator : MonoBehaviour {
     const string IP_ADDRESS = "127.0.0.1";
     const int PORT = 8888;
 
     public int captureID = 0;
-    Thread mainThread;
+    private CvCapture capture;
+
+    private byte[] getData() {
+        Mat mat = new Mat(capture.QueryFrame());
+        return mat.ToBytes(".jpg");
+    }
+
+#if WINDOWS_UWP
+    private Task mainTask;
+
+    void Awake() {
+        mainTask = new Task(run);
+        mainTask.Start();
+    }
+
+    void OnApplicationQuit() {
+        mainTask = null;
+    }
+
+    private async void run() {
+        StreamSocket socket = new StreamSocket();
+        await socket.ConnectAsync(new HostName(IP_ADDRESS), "" + PORT);
+        Stream stream = socket.InputStream.AsStreamForRead();
+
+        capture = CvCapture.FromCamera(captureID);
+        while (mainTask != null) {
+            byte[] imageData = getData();
+            stream.Write(imageData, 0, imageData.Length);
+            stream.Flush();
+            await Task.Delay(TimeSpan.FromSeconds(0.01));
+        }
+    }
+#else
+    private Thread mainThread;
 
     void Awake() {
         mainThread = new Thread(run);
@@ -18,9 +59,7 @@ public class CaptureSimulator : MonoBehaviour {
     }
 
     void OnApplicationQuit() {
-        if (mainThread.IsAlive) {
-            mainThread.Abort();
-        }
+        mainThread = null;
     }
 
     private void run() {
@@ -29,10 +68,9 @@ public class CaptureSimulator : MonoBehaviour {
 
         NetworkStream networkStream = client.GetStream();
 
-        CvCapture capture = CvCapture.FromCamera(captureID);
+        capture = CvCapture.FromCamera(captureID);
         while (mainThread != null) {
-            Mat mat = new Mat(capture.QueryFrame());
-            byte[] imageData = mat.ToBytes(".jpg");
+            byte[] imageData = getData();
             networkStream.Write(imageData, 0, imageData.Length);
             networkStream.Flush();
             Thread.Sleep(10);
@@ -40,4 +78,5 @@ public class CaptureSimulator : MonoBehaviour {
 
         client.Close();
     }
+#endif
 }
--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using System.IO;
+using System;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+#if WINDOWS_UWP
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+#else
 using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System;
-using OpenCvSharp;
-using OpenCvSharp.CPlusPlus;
+#endif
 
 public class VideoManager : MonoBehaviour {
     static VideoManager videoManager = null;
@@ -18,16 +25,64 @@ public class VideoManager : MonoBehaviour {
 
     public byte[][] videos;
     public byte[] buffer;
-    private Thread mainThread;
+    
+    static public bool getFrame(int id, out Texture2D texture) {
+        if (videoManager.videos[id] != null) {
+            Mat mat = Mat.FromImageData(videoManager.videos[id], LoadMode.Color);
+            texture = new Texture2D(mat.Height, mat.Width, TextureFormat.RGB24, false, false);
+            texture.LoadImage(videoManager.videos[id]);
+            return true;
+        } else {
+            texture = null;
+            return false;
+        }
+    }
 
     void Awake() {
-		if (videoManager == null) {
+        if (videoManager == null) {
             videoManager = this;
         }
 
         videos = new byte[MAX_STUDENTS][];
         buffer = new byte[BUFFER_LEN];
 
+        startThread();
+    }
+
+#if WINDOWS_UWP
+    private Task mainTask;
+    
+    void startThread() {
+        mainTask = new Task(serverThread);
+        mainTask.Start();
+    }
+
+    void OnApplicationQuit() {
+        mainTask = null;
+    }
+
+    private async void serverThread() {
+        StreamSocketListener listener = new StreamSocketListener();
+        listener.ConnectionReceived += connectionReceived;
+        await listener.BindServiceNameAsync("" + PORT);
+    }
+
+    private async void connectionReceived(StreamSocketListener listener, StreamSocketListenerConnectionReceivedEventArgs args) {
+        Stream stream = args.Socket.InputStream.AsStreamForRead();
+
+        while (mainTask != null) {
+            int len = await stream.ReadAsync(buffer, 0, buffer.Length);
+            if (len == 0) {
+                break;
+            }
+            videos[0] = new byte[len];
+            Array.Copy(buffer, videos[0], len);
+        }
+    }
+#else
+    private Thread mainThread;
+
+    void startThread() {
         mainThread = new Thread(serverThread);
         mainThread.Start();
 	}
@@ -63,16 +118,5 @@ public class VideoManager : MonoBehaviour {
 
         client.Close();
     }
-
-    static public bool getFrame(int id, out Texture2D texture) {
-        if (videoManager.videos[id] != null) {
-            Mat mat = Mat.FromImageData(videoManager.videos[id], LoadMode.Color);
-            texture = new Texture2D(mat.Height, mat.Width, TextureFormat.RGB24, false, false);
-            texture.LoadImage(videoManager.videos[id]);
-            return true;
-        } else {
-            texture = null;
-            return false;
-        }
-    }
+#endif
 }
